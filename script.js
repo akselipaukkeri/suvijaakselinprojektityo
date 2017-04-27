6a734d1081d02574051e2e8999c5b8ac4652f257
akselipaukkeri <akseli.paukkeri@gmail.com>
2017-04-27 17:05:50 +0300

    yritän saatan saa pelimaailmaa näkyvii, kohta onnistun

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('sky','putin.jpg');
-    //game.load.image('ground', 'assets/platform.png');
-    //game.load.image('star', 'assets/star.png');
-    //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('putin','putin.jpg');
+    game.load.image('ground', 'putin.jpg');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    console.log("jee")
 
 }
 var platforms;
@@ -13,7 +14,8 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+    game.add.sprite(0, 0, 'putin');
+    console.log("vittu")
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
@@ -43,4 +45,23 @@ function create() {
 }
 
 function update() {
+     var hitPlatform = game.physics.arcade.collide(player, platforms);
 }
+
+ // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+preload()
+create()
+update()
