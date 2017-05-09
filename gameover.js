d51ed17c12919a96e259b7eb6f8e8adcb6f3d2da
suvihamalainen <suvi.s.hamalainen@aalto.fi>
2017-05-09 15:02:12 +0300

    koko paska

--- gameover.js
+++ gameover.js
@@ -1,4 +1,17 @@
-var gameOverState = {
-    game.add.sprite(0, 0, 'putin');
-    game.add.text(16, 16, 'Game Over', { fontSize: '32px', fill: '#000' });
+var gameOver = function(game){}
+ 
+gameOver.prototype = {
+    
+  	create: function() {
+        var finalScore = this.game.state.states["TheGame"].score;
+  		this.game.add.sprite(0, 0, 'sky');
+  		this.game.add.sprite(70, 50, 'gameover');
+        this.game.add.text(170, 200, 'You scored: ' + this.finalScore, { fontSize: '40px', fill: '#000' });
+		var playButton = this.game.add.button(315,320,"play",this.playTheGame,this);
+		playButton.anchor.setTo(0.5,0.5);
+	},
+    
+	playTheGame: function() {
+		this.game.state.start("TheGame");
+	}
 }
